ed5ca24a087486f4d1a29e957bf734a0f924f5dd
Md. Tahmid Hossain <55309419+tahmid02016@users.noreply.github.com>
2023-01-25 22:48:29 +0600

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+"""Provides functionality to convert numbers into bangla words
+-------------------------------------------------------------------------------
+`num2bangla` is a module that provides functionality to convert numbers like `123` to Bangla words like `এক শত তেইশ`.
+"""
+
+__version_info__ = ('1', '0', '0')
+__version__ = '.'.join(__version_info__)
